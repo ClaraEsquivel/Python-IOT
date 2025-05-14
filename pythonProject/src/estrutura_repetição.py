f6b4100b6a705a81d3f7fb01da0ac3c8554cf0d2
Clara Esquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2025-05-14 09:39:11 -0300

    Add files via upload

---
+++ pythonProject/src/estrutura_repetição.py
@@ -0,0 +1,75 @@
+# Estrutura de dados de repetição
+
+# # Loop controlado
+# x = 1
+# while x <= 10:
+#     print(x)
+#     x += 1 #(x = x + +1)
+
+
+# # Loop infinito
+# while True:
+#     print(x)
+    
+
+# # Outro loop infinito
+# x = 2
+# while x == 2:   
+#     print(x)
+
+
+# # Tabuada de um número
+# num = int(input("Digite um número para ver a tabuada: "))
+# x = 1
+# while x <= 10:
+#     print(x * num)
+#     x += 1
+
+
+# # Tabuada com contador e acumulador
+# n = 1 #contador
+# soma = 0  #acumulador   
+# while n <= 10:
+#     num = int(input("Digite um número: "))
+#     soma = soma + num
+#     n += 1
+#     print("A soma dos números é:", soma)
+
+
+# # Interrupção de uma repetição
+# x = 1
+# while x <= 10:
+#     if x == 5:
+#         break
+#     x = x + 1
+#     print("O loop foi interrompido na interação:", x)
+
+
+# # Interrupção de uma repetição 2
+# x = 0
+# while True:
+#     num = int(input("Digite um número: "))
+#     if num == 0:
+#         break
+#     x = x + num
+# print(x)    
+
+
+# # Estrutura de repetição for
+# for i in range(1,6):
+#     print(i)
+
+# for char in "Python":
+#     print(char)
+
+
+# # Estrutura de repetição for com lista
+# lista = [1, 2, 3, 4, 5]
+# for i in lista:
+#     print(i)
+
+
+# # Estrutura de repetição for com string
+computador =  ['Monitor', 'Teclado', 'Mouse']
+for i in computador:
+    print(i)
